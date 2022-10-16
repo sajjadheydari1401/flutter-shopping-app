4a12e2dae74b8e6fa34fd0a7b87322bb92b99b7d
sajjadheydari1401 <sajjadheydari1401@gmail.com>
2022-10-16 20:08:04 +0330

    add product model & screens & widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,25 @@
 import 'package:flutter/material.dart';
 
+import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme =
+        ThemeData(fontFamily: 'Lato', primarySwatch: Colors.purple);
     return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShop'),
-      ),
-      body: Center(
-        child: Text('Let\'s build a shop!'),
-      ),
-    );
+        title: 'MyShop',
+        theme: theme.copyWith(
+          colorScheme: theme.colorScheme.copyWith(
+            secondary: Colors.deepOrange,
+          ),
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        });
   }
 }
